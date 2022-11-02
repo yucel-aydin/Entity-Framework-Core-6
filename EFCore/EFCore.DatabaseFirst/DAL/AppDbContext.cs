cb7654b79c93bc6210e200fb561a0f32a3f27dc3
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-11-02 13:47:38 +0300

    DbContext sınıfı product entity oluştuldu

---
+++ EFCore/EFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DatabaseFirst.DAL
+{
+    //Ef DbContext sınıfından miras alan dbcontext imiz
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //https://www.connectionstrings.com/sql-server/ burada tüm db ler için connection stringler bulunabilir.
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EfCoreDBFirst;User ID=sa;Password=951753;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
--- EFCore/EFCore.DatabaseFirst/Program.cs
+++ EFCore/EFCore.DatabaseFirst/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EFCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context =new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+	products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    }) ;
+}
