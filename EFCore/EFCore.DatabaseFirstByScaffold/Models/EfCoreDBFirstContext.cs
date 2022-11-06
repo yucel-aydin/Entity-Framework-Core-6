6cd7b8e4fe7833c0b6593fbfc9c81d5a634658f4
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-11-06 15:08:44 +0300

    Scaffold örneği eklendi

---
+++ EFCore/EFCore.DatabaseFirstByScaffold/Models/EfCoreDBFirstContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCore.DatabaseFirstByScaffold.Models
+{
+    public partial class EfCoreDBFirstContext : DbContext
+    {
+        public EfCoreDBFirstContext()
+        {
+        }
+
+        public EfCoreDBFirstContext(DbContextOptions<EfCoreDBFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EfCoreDBFirst;User ID=sa;Password=951753;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- EFCore/EFCore.DatabaseFirstByScaffold/Program.cs
+++ EFCore/EFCore.DatabaseFirstByScaffold/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EFCore.DatabaseFirstByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new EfCoreDBFirstContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Stock}- {p.Price}");
+    });
+}
+//Scaffold-DbContext "Data Source=.;Initial Catalog=EfCoreDBFirst;User ID=sa;Password=951753;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models 
