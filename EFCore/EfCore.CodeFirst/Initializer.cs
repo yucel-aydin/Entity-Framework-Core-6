ce5fb2bac46cdb9350218ac185557df8fa64ca2e
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-11-06 16:32:22 +0300

    appsettings.json dosyasını kullanabilmek için, Microsoft.Extensions.Configuration Microsoft.Extensions.Configuration.FileExtensions Microsoft.Extensions.Configuration.Json paketleri yüklendi.

---
+++ EFCore/EfCore.CodeFirst/Initializer.cs
@@ -0,0 +1,28 @@
+﻿using EfCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.CodeFirst
+{
+    public class Initializer
+    {
+        public static IConfigurationRoot Configuration;
+
+        public static void Build()
+        {
+            // optional : bu dosya olabilirde olmayabilirde.
+            // reloadOnChange : her değişiklik yapıldığında yeniden yüklensinmi
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+       
+        }
+    }
+}
--- EFCore/EfCore.CodeFirst/Program.cs
+++ EFCore/EfCore.CodeFirst/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EfCore.CodeFirst;
+using EfCore.CodeFirst.DAL;
+
+//appsettings.json dosyamızı okunabilir hale getirir.
+Initializer.Build();
+
+
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - {p.Name} - {p.Stock}- {p.Price}");
+    });
+}
